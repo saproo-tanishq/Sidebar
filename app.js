0e88b4ca0bb353d5f75c748cebea8ea2720ab105
saproo-tanishq <saprootanishq@gmail.com>
2021-06-14 12:16:18 +0530

    Files Added

---
+++ app.js
@@ -0,0 +1,19 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closebtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+    // console.log(sidebar.classList);
+    // if(sidebar.classList.contains("show-sidebar")){
+    //     sidebar.classList.remove("show-sidebar")
+    // }
+    // else{
+    //     sidebar.classList.add("show-sidebar")
+    // }
+    sidebar.classList.toggle("show-sidebar");
+
+});
+
+closebtn.addEventListener("click", function(){
+    sidebar.classList.remove("show-sidebar");
+})
